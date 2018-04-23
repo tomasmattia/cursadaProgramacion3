dc4d891e98a2de89d1aa432e6614235d4f8308f6
Tomas Mattia <tomasmattia@users.noreply.github.com>
2018-04-23 12:08:09 -0300

    Add files via upload

---
+++ clase05 - PhpMyAdmin/utn (1).sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-04-2018 a las 16:56:00
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `utn`
+--
+CREATE DATABASE IF NOT EXISTS `utn` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `utn`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `envios`
+--
+
+CREATE TABLE `envios` (
+  `Numero` int(11) NOT NULL,
+  `pNumero` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `envios`
+--
+
+INSERT INTO `envios` (`Numero`, `pNumero`, `Cantidad`) VALUES
+(100, 1, 500),
+(100, 2, 1500),
+(100, 3, 100),
+(101, 2, 55),
+(101, 3, 225),
+(102, 1, 600),
+(102, 3, 300);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `pNumero` int(11) NOT NULL,
+  `pNombre` varchar(30) DEFAULT NULL,
+  `Precio` float DEFAULT NULL,
+  `Tamaño` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`pNumero`, `pNombre`, `Precio`, `Tamaño`) VALUES
+(2, 'Cigarrillos', 45.89, 'Mediano'),
+(3, 'Gaseosa', 97.5, 'Grande'),
+(4, 'Chocolate', 25.35, 'Chico');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provedores`
+--
+
+CREATE TABLE `provedores` (
+  `Numero` int(11) NOT NULL,
+  `Nombre` varchar(30) DEFAULT NULL,
+  `Domicilio` varchar(50) DEFAULT NULL,
+  `Localidad` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `provedores`
+--
+
+INSERT INTO `provedores` (`Numero`, `Nombre`, `Domicilio`, `Localidad`) VALUES
+(100, 'Perez', 'Perón 876', 'Quilmes'),
+(101, 'Gimenez', 'Mitre 750', 'Avellaneda'),
+(102, 'Aguirre', 'Boedo 634', 'Bernal');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `envios`
+--
+ALTER TABLE `envios`
+  ADD PRIMARY KEY (`Numero`,`pNumero`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`pNumero`);
+
+--
+-- Indices de la tabla `provedores`
+--
+ALTER TABLE `provedores`
+  ADD PRIMARY KEY (`Numero`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `pNumero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `provedores`
+--
+ALTER TABLE `provedores`
+  MODIFY `Numero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
