6b013ed022ae1917cc7d892faf3dbabe6fb2cd4b
Tomas Mattia <tomasmattia@users.noreply.github.com>
2018-05-28 11:09:31 -0300

    Add files via upload

---
+++ lamparitas_bd.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-05-2018 a las 01:14:43
+-- Versión del servidor: 10.1.25-MariaDB
+-- Versión de PHP: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `lamparitas_bd`
+--
+CREATE DATABASE IF NOT EXISTS `lamparitas_bd` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `lamparitas_bd`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lamparitas`
+--
+
+CREATE TABLE `lamparitas` (
+  `tipo` varchar(50) NOT NULL,
+  `color` varchar(50) NOT NULL,
+  `precio` double NOT NULL,
+  `path` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
